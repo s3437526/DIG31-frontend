3483ea91353f51055c565079b802c08a2cb9dced
Alex <alexalduk@hotmail.com>
2021-05-03 17:07:35 +1000

    Basic implementation of top and side nav, and responsive

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Front end development for DIG31
 
 npm install --save lit-element lit-html
+
+TODO
+- Set fonts
+- Sidebar header close button (x) colour fix
+- Menu buttons (icons) have tooltip that says "Home"
--- src/components/va-app-header.js
+++ src/components/va-app-header.js
@@ -72,7 +72,6 @@ customElements.define('va-app-header', class AppHeader extends LitElement {
         box-shadow: 4px 0px 10px rgba(0,0,0,0.2);
         align-items: center;
       }
-      
 
       .app-header-main {
         flex-grow: 1;
@@ -99,6 +98,7 @@ customElements.define('va-app-header', class AppHeader extends LitElement {
       
       .hamburger-btn::part(base) {
         color: #fff;
+        font-size: 2.5em;
       }
 
       .app-top-nav {
@@ -113,21 +113,34 @@ customElements.define('va-app-header', class AppHeader extends LitElement {
         text-decoration: none;
         color: #fff;
       }
+
+      sl-drawer::part(panel){
+        background-color: rgb(94,85,107);
+        --size: 15em;
+      }
       
+      sl-drawer::part(overlay){
+        background: transparent;
+      }
+
+      .sidenav-content{
+        display:flex; 
+        flex-direction:column; 
+        align-items:center; 
+        height: 100%; 
+        justify-content: space-between;
+      }
+
+      .bottom-menus{
+        width: 100%;
+      }
+
       .app-side-menu-items a {
         display: block;
         padding: .5em;
         text-decoration: none;
         font-size: 1.3em;
-        color: #333;
-      }
-
-      .app-side-menu-logo {
-        width: 120px;
-        margin-bottom: 1em;
-        position: absolute;
-        top: 2em;
-        left: 1.5em;
+        color: var(--app-header-txt-color);
       }
 
       .page-title {
@@ -141,36 +154,26 @@ customElements.define('va-app-header', class AppHeader extends LitElement {
       .app-side-menu-items a.active {
         font-weight: bold;
       }
+      
+      sl-icon{
+        color:white;
+      }
 
-      #bell-icon, #alert-icon{
+      #bell-icon, #bell-icon-sidenav, #alert-icon, #alert-icon-sidenav, .bi-bell{
         color: gray;
       }
 
       sl-dropdown::part(panel){
         background-color: rgb(94,85,107);
-        position: relative;
-        right: -1px;
         border: none;
-        /* overflow-y: scroll; */
         max-height: 100vh;
         border-radius:  0 0 5px 5px;
       }
 
-      /* .dropdowns::part(base){ */
-      /* } */
-      /* sl-dropdown::part(base){ */
-        /* background:blue; */
-        /* position:relative; */
-        /* --size:6rem; */
-        /* height: 100vh; */
-        /* height:10%; */
-        /* overflow-y:hidden; */
-/* height:80%; */
-
-      /* } */
-
-      /* sl-menu::part(base){
-      } */
+      sl-menu.left-menu::part(base){
+        border: 1px solid #fff;
+        border-radius: 5px;
+      }
 
       sl-menu-item::part(base){
         color: #fff;
@@ -179,6 +182,11 @@ customElements.define('va-app-header', class AppHeader extends LitElement {
       sl-menu-item::part(base):hover{
         color: rgb(94,85,107);
       } 
+
+      sl-avatar::part(base){
+        --size: 2rem;
+        color:white;
+      }
       
       .dropdown-icon{
         font-size: 2rem;
@@ -226,12 +234,9 @@ customElements.define('va-app-header', class AppHeader extends LitElement {
         margin-right:1.5rem;
       }
 
-      /* sl-icon::part(base){ */
-        /* display: flex;
-        justify-content: center;
-        align-items: center;
-        size: 3.5rem; */
-      /* } */
+      .app-side-nav{
+        display:none;
+      }
 
       /* RESPONSIVE - MOBILE ------------------- */
       @media all and (max-width: 768px){       
@@ -239,98 +244,179 @@ customElements.define('va-app-header', class AppHeader extends LitElement {
         .app-top-nav {
           display: none;
         }
+
+        .app-side-nav{
+          display:flex;
+          align-items: center;
+          justify-content: space-evenly;
+        }
       }
 
     </style>
 
-    <header class="app-header">
-      <!-- <sl-icon-button class="hamburger-btn" name="list" @click="${this.hamburgerClick}" style="font-size: 1.5em;"></sl-icon-button>       
-      
-      <div class="app-header-main">
+    <header class="app-header" style="display:flex; justify-content: space-between; align-items:center;">
+      <div class="left-navs">
+        <sl-icon-button class="hamburger-btn" name="list" @click="${this.hamburgerClick}"></sl-icon-button>       
+      </div>
+
+      <!-- <div class="app-header-main">
         ${this.title ? html`
           <h1 class="page-title">${this.title}</h1>
         `: ``}
-        <slot></slot> -->
-      </div>
-
-      <nav class="app-top-nav">
-        <!-- <a href="/" @click="${anchorRoute}">Home</a>         -->
-        <a slot="trigger" id="bell-icon" href="#" style="display: flex; align-items: center;" @click="${(e) => e.preventDefault()}">
-             <sl-icon slot="icon" name="bell" style="font-size: 2rem;"></sl-icon>
-          </a>
-        <a slot="trigger" id="alert-icon" href="#" style="display: flex; align-items: center;" @click="${(e) => e.preventDefault()}">
-             <sl-icon slot="icon" name="exclamation-circle" style="font-size: 2rem;"></sl-icon>
-          </a>
-        <sl-dropdown>
+        <slot></slot>
+      </div> -->
+      <div class="right-navs">
+        <nav class="app-top-nav">
+          <!-- <a href="/" @click="${anchorRoute}">Home</a>         -->
           <a slot="trigger" href="#" style="display: flex; align-items: center;" @click="${(e) => e.preventDefault()}">
-             <sl-icon slot="icon" name="gear-fill" style="font-size: 1.9rem;"></sl-icon>
-          </a>
-          <sl-menu>            
-            <sl-menu-item @click="${() => gotoRoute('/profile')}"><sl-icon class="dropdown-icon" slot="prefix" name="wifi"></sl-icon>System Status</sl-menu-item>
-            <sl-menu-item @click="${() => gotoRoute('/editProfile')}"><sl-icon class="dropdown-icon" slot="prefix" name="list-ul"></sl-icon>Logs</sl-menu-item>
-          </sl-menu>
-        </sl-dropdown>
-        <sl-dropdown class="dropdowns">
-          <a slot="trigger" href="#" @click="${(e) => e.preventDefault()}">
-            <sl-avatar style="--size: 1.9rem;" image=${(this.user && this.user.avatar) ? `${App.apiBase}/images/${this.user.avatar}` : ``}></sl-avatar> ${this.user && this.user.firstName}
-          </a>
-          <sl-menu>            
-          ${Auth.currentUser.accessLevel == 2? html`
-            <sl-menu-item @click="${() => gotoRoute('/profile')}">
-              <sl-icon class="dropdown-icon" slot="prefix" name="person"></sl-icon>
-              <sl-icon class="settings-icon" slot="prefix" name="plus"></sl-icon>
-                Register User
-            ` : ``}
-            </sl-menu-item>
-            <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
-              <sl-icon class="dropdown-icon" slot="prefix" name="person"></sl-icon>
-              <sl-icon class="add-icon" slot="prefix" name="gear-fill"></sl-icon>
-                Manage Account
-            </sl-menu-item>
-            ${Auth.currentUser.accessLevel == 2? html`
-            <sl-menu-divider></sl-menu-divider>
+              <sl-icon id="bell-icon" slot="icon" name="bell" style="font-size: 2rem;"></sl-icon>
+            </a>
+          <a slot="trigger" href="#" style="display: flex; align-items: center;" @click="${(e) => e.preventDefault()}">
+              <sl-icon id="alert-icon" slot="icon" name="exclamation-circle" style="font-size: 2rem;"></sl-icon>
+            </a>
+          <sl-dropdown>
+            <a slot="trigger" href="#" style="display: flex; align-items: center;" @click="${(e) => e.preventDefault()}">
+              <sl-icon slot="icon" name="gear-fill" style="font-size: 1.9rem;"></sl-icon>
+            </a>
+            <sl-menu>            
+              <sl-menu-item @click="${() => gotoRoute('/profile')}"><sl-icon class="dropdown-icon" slot="prefix" name="wifi"></sl-icon>System Status</sl-menu-item>
+              <sl-menu-item @click="${() => gotoRoute('/editProfile')}"><sl-icon class="dropdown-icon" slot="prefix" name="list-ul"></sl-icon>Logs</sl-menu-item>
+            </sl-menu>
+          </sl-dropdown>
+          <sl-dropdown distance="0" class="dropdowns">
+            <a slot="trigger" href="#" @click="${(e) => e.preventDefault()}">
+              <sl-avatar image=${(this.user && this.user.avatar) ? `${App.apiBase}/images/${this.user.avatar}` : ``}></sl-avatar> ${this.user && this.user.firstName}
+            </a>
+            <sl-menu>            
+            ${Auth.currentUser.accessLevel == 2 ? html`
               <sl-menu-item @click="${() => gotoRoute('/profile')}">
-                <sl-icon class="dropdown-icon" slot="prefix" name="house-door"></sl-icon>
+                <sl-icon class="dropdown-icon" slot="prefix" name="person"></sl-icon>
                 <sl-icon class="settings-icon" slot="prefix" name="plus"></sl-icon>
-                  Register Place
+                  Register User
+              ` : ``}
               </sl-menu-item>
-            <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
-              <sl-icon class="dropdown-icon" slot="prefix" name="house-door"></sl-icon>
-              <sl-icon class="manage-place" slot="prefix" name="gear-fill"></sl-icon>
-                Manage Place
-            </sl-menu-item>
-            <sl-menu-divider></sl-menu-divider>
-            <sl-menu-item @click="${() => gotoRoute('/profile')}">
-              <sl-icon class="dropdown-icon" slot="prefix" name="broadcast"></sl-icon>
-              <sl-icon class="manage-device" slot="prefix" name="plus"></sl-icon>
-                Register Device
-            </sl-menu-item>
-            <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
-              <sl-icon class="dropdown-icon" slot="prefix" name="broadcast"></sl-icon>
-              <sl-icon class="add-device" slot="prefix" name="gear-fill"></sl-icon>
-                Manage Device
-            </sl-menu-item>
-            <sl-menu-divider></sl-menu-divider>
-            ` : ``}
-            <sl-menu-item @click="${() => Auth.signOut()}">
-              <sl-icon class="signout-icon" slot="prefix" name="box-arrow-right"></sl-icon>
-                Sign Out
-            </sl-menu-item>
-          </sl-menu>
-        </sl-dropdown>
-      </nav>
+              <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
+                <sl-icon class="dropdown-icon" slot="prefix" name="person"></sl-icon>
+                <sl-icon class="add-icon" slot="prefix" name="gear-fill"></sl-icon>
+                  Manage Account
+              </sl-menu-item>
+              ${Auth.currentUser.accessLevel == 2 ? html`
+              <sl-menu-divider></sl-menu-divider>
+                <sl-menu-item @click="${() => gotoRoute('/profile')}">
+                  <sl-icon class="dropdown-icon" slot="prefix" name="house-door"></sl-icon>
+                  <sl-icon class="settings-icon" slot="prefix" name="plus"></sl-icon>
+                    Register Place
+                </sl-menu-item>
+              <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
+                <sl-icon class="dropdown-icon" slot="prefix" name="house-door"></sl-icon>
+                <sl-icon class="manage-place" slot="prefix" name="gear-fill"></sl-icon>
+                  Manage Place
+              </sl-menu-item>
+              <sl-menu-divider></sl-menu-divider>
+              <sl-menu-item @click="${() => gotoRoute('/profile')}">
+                <sl-icon class="dropdown-icon" slot="prefix" name="broadcast"></sl-icon>
+                <sl-icon class="manage-device" slot="prefix" name="plus"></sl-icon>
+                  Register Device
+              </sl-menu-item>
+              <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
+                <sl-icon class="dropdown-icon" slot="prefix" name="broadcast"></sl-icon>
+                <sl-icon class="add-device" slot="prefix" name="gear-fill"></sl-icon>
+                  Manage Device
+              </sl-menu-item>
+              <sl-menu-divider></sl-menu-divider>
+              ` : ``}
+              <sl-menu-item @click="${() => Auth.signOut()}">
+                <sl-icon class="signout-icon" slot="prefix" name="box-arrow-right"></sl-icon>
+                  Sign Out
+              </sl-menu-item>
+            </sl-menu>
+          </sl-dropdown>
+        </nav>
+      </div>
     </header>
 
     <sl-drawer class="app-side-menu" placement="left">
-      <!-- <img class="app-side-menu-logo" src="/images/logo.svg">
-      <nav class="app-side-menu-items">
-        <a href="/" @click="${this.menuClick}">Home</a>
-        <a href="/hairdressers" @click="${this.menuClick}">Find a Hairdresser</a>
-        <a href="/haircute" @click="${this.menuClick}">Find a Haircut</a>
-        <a href="/favouriteHaircuts" @click="${this.menuClick}">Hairdressers</a>
-        <a href="/profile" @click="${this.menuClick}">Profile</a>
-        <a href="#" @click="${() => Auth.signOut()}">Sign Out</a> -->
-      </nav>  
+      <div class="sidenav-content">
+        <div class="top-menus">
+          <!-- <img class="app-side-menu-logo" src="/images/logo.svg"> -->
+          <nav class="app-side-menu-items">
+            <a href="/" @click="${this.menuClick}">Dashboard</a>
+            <a href="/hairdressers" @click="${this.menuClick}">Find a Hairdresser</a>
+            <a href="/haircute" @click="${this.menuClick}">Find a Haircut</a>
+            <a href="/favouriteHaircuts" @click="${this.menuClick}">Hairdressers</a>
+            <a href="/profile" @click="${this.menuClick}">Profile</a>
+            <a href="#" @click="${() => Auth.signOut()}">Sign Out</a>
+          </nav>  
+        </div>
+        <div class="bottom-menus">
+          <nav class="app-side-nav">
+            <!-- <a href="/" @click="${anchorRoute}">Home</a>         -->
+            <a slot="trigger" href="#" style="display: flex; align-items: center;" @click="${(e) => e.preventDefault()}">
+                <sl-icon id="bell-icon-sidenav" slot="icon" name="bell" style="font-size: 2rem;"></sl-icon>
+              </a>
+            <a slot="trigger" href="#" style="display: flex; align-items: center;" @click="${(e) => e.preventDefault()}">
+                <sl-icon id="alert-icon-sidenav" slot="icon" name="exclamation-circle" style="font-size: 2rem;"></sl-icon>
+              </a>
+            <sl-dropdown skidding="-109" distance="10">
+              <a slot="trigger" href="#" style="display: flex; align-items: center;" @click="${(e) => e.preventDefault()}">
+                <sl-icon slot="icon" name="gear-fill" style="font-size: 2rem;"></sl-icon>
+              </a>
+              <sl-menu class="left-menu">            
+                <sl-menu-item @click="${() => gotoRoute('/profile')}"><sl-icon class="dropdown-icon" slot="prefix" name="wifi"></sl-icon>System Status</sl-menu-item>
+                <sl-menu-item @click="${() => gotoRoute('/editProfile')}"><sl-icon class="dropdown-icon" slot="prefix" name="list-ul"></sl-icon>Logs</sl-menu-item>
+              </sl-menu>
+            </sl-dropdown>
+            <sl-dropdown skidding="-171" distance="10" class="dropdowns-left">
+              <a slot="trigger" href="#" @click="${(e) => e.preventDefault()}">
+                <sl-avatar image=${(this.user && this.user.avatar) ? `${App.apiBase}/images/${this.user.avatar}` : ``}>
+              </a>
+              <sl-menu class="left-menu">            
+                ${Auth.currentUser.accessLevel == 2 ? html`
+                  <sl-menu-item @click="${() => gotoRoute('/profile')}">
+                    <sl-icon class="dropdown-icon" slot="prefix" name="person"></sl-icon>
+                    <sl-icon class="settings-icon" slot="prefix" name="plus"></sl-icon>
+                      Register User
+                  ` : ``}
+                  </sl-menu-item>
+                  <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
+                    <sl-icon class="dropdown-icon" slot="prefix" name="person"></sl-icon>
+                    <sl-icon class="add-icon" slot="prefix" name="gear-fill"></sl-icon>
+                      Manage Account
+                  </sl-menu-item>
+                  ${Auth.currentUser.accessLevel == 2 ? html`
+                  <sl-menu-divider></sl-menu-divider>
+                    <sl-menu-item @click="${() => gotoRoute('/profile')}">
+                      <sl-icon class="dropdown-icon" slot="prefix" name="house-door"></sl-icon>
+                      <sl-icon class="settings-icon" slot="prefix" name="plus"></sl-icon>
+                        Register Place
+                    </sl-menu-item>
+                  <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
+                    <sl-icon class="dropdown-icon" slot="prefix" name="house-door"></sl-icon>
+                    <sl-icon class="manage-place" slot="prefix" name="gear-fill"></sl-icon>
+                      Manage Place
+                  </sl-menu-item>
+                  <sl-menu-divider></sl-menu-divider>
+                  <sl-menu-item @click="${() => gotoRoute('/profile')}">
+                    <sl-icon class="dropdown-icon" slot="prefix" name="broadcast"></sl-icon>
+                    <sl-icon class="manage-device" slot="prefix" name="plus"></sl-icon>
+                      Register Device
+                  </sl-menu-item>
+                  <sl-menu-item @click="${() => gotoRoute('/editProfile')}">
+                    <sl-icon class="dropdown-icon" slot="prefix" name="broadcast"></sl-icon>
+                    <sl-icon class="add-device" slot="prefix" name="gear-fill"></sl-icon>
+                      Manage Device
+                  </sl-menu-item>
+                  <sl-menu-divider></sl-menu-divider>
+                  ` : ``}
+                  <sl-menu-item @click="${() => Auth.signOut()}">
+                    <sl-icon class="signout-icon" slot="prefix" name="box-arrow-right"></sl-icon>
+                      Sign Out
+                  </sl-menu-item>
+              </sl-menu>
+            </sl-dropdown>
+          </nav>
+        </div>
+      </div>
     </sl-drawer>
     `
   }
